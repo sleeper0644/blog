6c9918f6c6279d78b5be64f11108c5b4d21d1042
SERGEY FEDORCHUK WORK PC <SLEEPER0644@GMAIL.COM>
2025-01-02 14:17:20 +0200

    first commit

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php
+$title = 'MY BLOG :: ABOUT';
+$post = ' <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci recusandae itaque temporibus architecto, qui sapiente quisquam blanditiis illo praesentium illum, quia eum officia saepe iusto ullam quibusdam, esse aliquam ratione.</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci recusandae itaque temporibus architecto, qui sapiente quisquam blanditiis illo praesentium illum, quia eum officia saepe iusto ullam quibusdam, esse aliquam ratione.</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci recusandae itaque temporibus architecto, qui sapiente quisquam blanditiis illo praesentium illum, quia eum officia saepe iusto ullam quibusdam, esse aliquam ratione.</p>
+';
+
+$recent_posts = [
+    1 => [
+        'title' => 'An item',
+        'slug' => lcfirst(str_replace(' ','-','An item'))
+    ],
+    2 => [
+        'title' => 'A second item',
+        'slug' => lcfirst(str_replace(' ','-','A second item'))
+    ],
+    3 => [
+        'title' => 'A third item',
+        'slug' => lcfirst(str_replace(' ','-','A third item'))
+    ],
+];
+
+require 'about.tpl.php';
+
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$title = 'MY BLOG :: HOME';
+$posts = [
+    1 => [
+        'title' => 'Title 1',
+        'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+        'slug' => 'title-1'
+    ],
+    2 => [
+        'title' => 'Title 2',
+        'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+        'slug' => 'title-2'
+    ],
+    3 => [
+        'title' => 'Title 3',
+        'desc' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+        'slug' => 'title-3'
+    ]
+];
+
+$recent_posts = [
+    1 => [
+        'title' => 'An item',
+        'slug' => lcfirst(str_replace(' ','-','An item'))
+    ],
+    2 => [
+        'title' => 'A second item',
+        'slug' => lcfirst(str_replace(' ','-','A second item'))
+    ],
+    3 => [
+        'title' => 'A third item',
+        'slug' => lcfirst(str_replace(' ','-','A third item'))
+    ],
+];
+
+require 'index.tpl.php';
+
