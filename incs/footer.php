509e9fe7094c141e61f95b61c67ef58990c19f7f
sergey fedorchuk work_notebook <sleeper0644gmail.com>
2025-01-05 14:54:45 +0200

    dell folder incs from root

--- incs/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer ">
-           <div class="p-3 bg-dark text-white text-center">
-                <p>&copy; Copyright <?= date('Y')?></p>
-           </div>
-        </footer>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
