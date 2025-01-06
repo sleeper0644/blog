0966efd370ab438023bcdc17e14c6f52e57338f3
sergey fedorchuk work_notebook <sleeper0644gmail.com>
2025-01-06 09:50:05 +0200

    create app strucrure

---
+++ core/router.php
@@ -0,0 +1,31 @@
+<?php
+
+require CONFIG . "/routes.php";
+
+$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'],'/');
+
+if (array_key_exists($uri,$routes))
+{
+    if (file_exists(CONTROLLERS . "/{$routes[$uri]}"))
+    {
+        require CONTROLLERS . "/{$routes[$uri]}";
+    } else {
+        abort();
+    }
+} else {
+    abort();
+}
+/*
+if ($uri === "")
+{
+    require CONTROLLERS . "/index.php";
+} elseif ($uri == "about")
+{
+    require CONTROLLERS . "/about.php";
+}elseif ($uri == "post") {
+    //require CONTROLLERS . "/about.php";
+    dd('show post');
+} else {
+    abort();
+}
+*/
